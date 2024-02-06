d59b222684806a67028da70f4fdbbec643090f89
WhaleFell <whalefall9420@outlook.com>
2024-02-06 19:10:45 +0800

    🐞 fix: fix frontend

--- app/assets/Home-77XVlXgQ.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r as t,o as n,c as r,t as a,u as s}from"./index-7zKG91cj.js";const l=o({__name:"Home",setup(c){const e=t("Home");return(m,p)=>(n(),r("div",null,a(s(e)),1))}});export{l as default};
